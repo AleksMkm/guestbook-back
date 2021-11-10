36197b662df20d376aaf4e73c402a73f04a4a164
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2021-11-10 09:41:29 +0200

    Allow underscore in name

--- routes/api/records/validation.js
+++ routes/api/records/validation.js
@@ -2,17 +2,28 @@ const Joi = require('joi')
 const { HttpCode } = require('../../../helpers/constants')
 
 const schemaCreateRecord = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  message: Joi.string().required()
+  name: Joi.string().pattern(/^\w+$/).required().messages({
+    'string.pattern.base': 'Only latin characters, numbers and underscore are allowed',
+    'any.required': 'Field is mandatory',
+    'string.empty': 'Can\'t be blank'
+  }),
+  message: Joi.string().required().messages({
+    'any.required': 'Field is mandatory',
+    'string.empty': 'Can\'t be blank'
+  })
 })
 
 const validate = (schema, obj, next) => {
   const { error } = schema.validate(obj)
   if (error) {
-    const [{ message }] = error.details
+    const [{ context, message }] = error.details
+    console.log('edt', error.details)
     return next({
       status: HttpCode.BAD_REQUEST,
-      message: `Field ${message.replace(/"/g, '')}`
+      message: {
+        field: context.label,
+        message: message
+      }
     })
   }
   next()
